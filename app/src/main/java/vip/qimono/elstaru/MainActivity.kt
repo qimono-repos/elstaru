b9ec93c94a24bacf7efd37d861fed87cad43d282
Qimono <albert.tan.2.pi.0@icloud.com>
2026-09-12 06:57:58 -0300

    feat: update message for brand

--- app/src/main/java/vip/qimono/elstaru/MainActivity.kt
+++ app/src/main/java/vip/qimono/elstaru/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             ElstaruTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Qimono",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "say Hello to $name!",
         modifier = modifier
     )
 }

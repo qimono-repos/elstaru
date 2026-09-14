294691ce51805d3e5c1cc45633bfc5dd39ba5346
qimono_at_w11 <albert@qimono>
2026-09-14 09:24:02 -0300

    feat: add imports for Jetpack Compose

--- app/src/main/java/vip/qimono/elstaru/MainActivity.kt
+++ app/src/main/java/vip/qimono/elstaru/MainActivity.kt
@@ -4,12 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfoV2
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import android.compose.ui.text.style.TextAlign
+import android.compose.ui.unit.sp
+
+import androidx.window.core.layout.WindowWidthSizeClass
+
+
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import vip.qimono.elstaru.ui.theme.ElstaruTheme
 
